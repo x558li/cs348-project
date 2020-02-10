083295c7a448da2be9c4149a04ddd7f361d6c5fb
Xinye Li <x558li@uwaterloo.ca>
2020-02-10 18:29:26 -0500

    FE - check if username exists before login

--- cs348_fullstack_work/src/main/webapp/js/logIn.js
+++ cs348_fullstack_work/src/main/webapp/js/logIn.js
@@ -2,13 +2,32 @@ var app = angular.module("LogInManagement", []);
 
 app.controller("LogInController", function($scope, $http, $rootScope) {
 	$scope.logInWarning = false;
-	
+	$scope.wrongUserWarning = false;
 	$scope.user = {};
 
 	$scope.logInUser = function() {
 		$scope.user.username = $scope.inputUsername;
 		$scope.user.password = $scope.inputPassword;
-		logInUser($scope.user);
+		checkUserExists($scope.user); //check -> login	
+	}
+
+	function checkUserExists(user) {
+		$http({
+            method : "GET",
+            url : '/checkUsernameExist/' + user.username,
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        }).then(function successCallback(response) {
+            if (response.data == true) {
+            	$scope.wrongUserWarning = false;
+            	logInUser(user);
+            } else {
+            	$scope.wrongUserWarning = true;
+            }
+        }, function errorCallback(response) {
+            console.log("ERROR: logInUser");
+        });
 	}
 
 	function logInUser(user) {
