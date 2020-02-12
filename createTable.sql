d134f8fd23abc74d3ea3848bee960a089cc0204c
x558li <35274349+x558li@users.noreply.github.com>
2020-02-11 20:38:47 -0500

    Update createTable.sql
    change schema, add user_type, fix dependencies

--- createTable.sql
+++ createTable.sql
@@ -27,27 +27,80 @@ INSERT INTO  VehicleInfo values (5, '2019',1,'X3 M40i',8.7);
 create table User
 (
 	uid int primary key auto_increment, 
-	username varchar(16) UNIQUE NOT NULL, 
-	password varchar(16) NOT NULL
+	username varchar(16) UNIQUE NOT NULL,
+	password varchar(16) NOT NULL,
+    user_type varchar(16) NOT NULL
 );
 
-INSERT INTO  User values (1, 'w22yuan','yuanwei');
-INSERT INTO  User values (2, 'h93yang','yaohaochen');
-INSERT INTO  User values (3, 'x6lian','lianxin');
-INSERT INTO  User values (4, 'x558li','nancy');
-INSERT INTO  User values (5, 'manufacturer0','cs348');
-INSERT INTO  User values (6, 'manufacturer1','cs348');
-INSERT INTO  User values (7, 'manufacturer2','cs348');
-INSERT INTO  User values (8, 'customer0','cs348');
-INSERT INTO  User values (9, 'customer1','cs348');
-INSERT INTO  User values (10, 'customer2','cs348');
+/*DELIMITER //
+
+create trigger CheckUserType
+before insert on User
+for each row
+BEGIN 
+	IF (NEW.user_type not in ('Admin','Manufacturer','Buyer')) THEN SET NEW.user_type = NULL;
+    END IF;
+END;//
+
+DELIMITER ;*/
+
+INSERT INTO  User values (1, 'w22yuan','yuanwei', 'Admin');
+INSERT INTO  User values (2, 'h93yang','yaohaochen', 'Admin');
+INSERT INTO  User values (3, 'x6lian','lianxin', 'Admin');
+INSERT INTO  User values (4, 'x558li','nancy', 'Admin');
+INSERT INTO  User values (5, 'manufacturer0','cs348', 'Manufacturer');
+INSERT INTO  User values (6, 'manufacturer1','cs348', 'Manufacturer');
+INSERT INTO  User values (7, 'manufacturer2','cs348', 'Manufacturer');
+INSERT INTO  User values (8, 'customer0','cs348', 'Buyer');
+INSERT INTO  User values (9, 'customer1','cs348', 'Buyer');
+INSERT INTO  User values (10, 'customer2','cs348', 'Buyer');
+
+create table Admin
+(	
+	uid int UNIQUE,
+    user_type varchar(16) CHECK (user_type = 'Admin'),
+	foreign key (uid) REFERENCES User(uid)
+);
+
+INSERT INTO  Admin values (1, 'Admin');
+INSERT INTO  Admin values (2, 'Admin');
+INSERT INTO  Admin values (3, 'Admin');
+INSERT INTO  Admin values (4, 'Admin'); 
+
+create table Manufacturer
+(
+	uid int UNIQUE, 
+	pid int UNIQUE,
+    user_type varchar(16) not null,
+    CHECK (user_type in ('Manufacturer')),
+	foreign key (uid) REFERENCES User(uid)
+);
+
+
+INSERT INTO  Manufacturer values (5, 1, 'Manufacturer');
+INSERT INTO  Manufacturer values (6, 2, 'Manufacturer');
+INSERT INTO  Manufacturer values (7, 3, 'Manufacturer');
+
+create table Buyer
+(	
+	uid int UNIQUE, 
+	cid int UNIQUE,
+    user_type varchar(16) CHECK (user_type = 'Buyer'),
+	foreign key (uid) REFERENCES User(uid)
+);
+
+INSERT INTO  Buyer values (8, 1, 'Buyer');
+INSERT INTO  Buyer values (9, 2, 'Buyer');
+INSERT INTO  Buyer values (10, 3, 'Buyer');
+
 
 create table Produce
 (
 	pid int,
 	vid int, 
 	PRIMARY KEY (pid, vid),
-	foreign key (vid) REFERENCES VehicleInfo(vid)
+	foreign key (vid) REFERENCES VehicleInfo(vid),
+    foreign key (pid) REFERENCES Manufacturer(pid)
 );
 
 
@@ -58,13 +111,13 @@ INSERT INTO Produce values (2, 4);
 INSERT INTO Produce values (3, 5);
 
 
-
 create table Cart
 (
 	cid int, 
 	vid int, 
     PRIMARY KEY (cid, vid),
-	foreign key (vid) REFERENCES VehicleInfo(vid)
+	foreign key (vid) REFERENCES VehicleInfo(vid),
+    foreign key (cid) REFERENCES Buyer(cid)
 );
 
 INSERT INTO Cart values (1, 1);
@@ -76,43 +129,4 @@ INSERT INTO Cart values (3, 1);
 INSERT INTO Cart values (3, 3);
 
 
-create table Admin
-(	
-	uid int UNIQUE,
-	username varchar(16) UNIQUE NOT NULL, 
-	password varchar(16) NOT NULL,
-	foreign key (uid) REFERENCES User(uid)
-);
 
-INSERT INTO  Admin values (1);
-INSERT INTO  Admin values (2);
-INSERT INTO  Admin values (3);
-INSERT INTO  Admin values (4); 
-
-create table Manufacturer
-(
-	uid int UNIQUE, 
-	pid int UNIQUE,
-	username varchar(16) UNIQUE NOT NULL, 
-	password varchar(16) NOT NULL,
-	foreign key (uid) REFERENCES User(uid), 
-	foreign key (pid) REFERENCES Produce(pid)
-);
-
-INSERT INTO  Manufacturer values (5, 1);
-INSERT INTO  Manufacturer values (6, 2);
-INSERT INTO  Manufacturer values (7, 3);
-
-create table Buyer
-(	
-	uid int UNIQUE, 
-	cid int UNIQUE,
-	username varchar(16) UNIQUE NOT NULL, 
-	password varchar(16) NOT NULL,
-	foreign key (uid) REFERENCES User(uid),
-	foreign key (cid) REFERENCES Cart(cid)
-);
-
-INSERT INTO  Buyer values (8, 1);
-INSERT INTO  Buyer values (9, 2);
-INSERT INTO  Buyer values (10, 3);
